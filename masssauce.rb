ef7f75e02023b20cd95a389d8aa52b8ec0eaef69
flan <flan@flande.re>
2017-02-24 04:44:14 +0100

    Separate fetching from views

--- masssauce.rb
+++ masssauce.rb
@@ -3,7 +3,57 @@ require 'sinatra'
 require_relative './utils'
 include ERB::Util
 
-index = -> { erb :index }
+get '/' do
+  erb :index
+end
 
-get '/', &index
-post '/', &index
+post '/' do
+  @results = {}
+  @errors = {}
+  @nosauce = {}
+  @notfound = []
+
+  return erb(:index) if not params[:urls]
+
+  params[:urls].split("\r\n").each do |url|
+    begin
+      images = find_images(url)
+      if images.empty? then
+        @notfound << url
+        next
+      end
+
+      images.each do |img|
+        s = saucenao(img)
+        s['results'].each do |r|
+          sim = r['header']['similarity']
+          next if sim.to_f < params[:min_similarity].to_f
+
+          urls = []
+          ids = {}
+
+          r['data'].each do |k, v|
+            if $base_urls.key? k then
+              urls << $base_urls[k] + v.to_s
+            elsif k.end_with? '_id' and k != 'member_id' then
+              ids[k] = v.to_s
+            end
+          end
+
+          @results[url] = {} if not @results.key? url
+          @results[url][img] = [] if not @results[url].key? img
+          @results[url][img] << {:similarity => sim, :urls => urls, :ids => ids}
+        end
+
+        unless @results.key? url and @results[url].key? img then
+          @nosauce[url] = [] if not @nosauce.key? url
+          @nosauce[url] << img
+        end
+      end
+    rescue StandardError => e
+      @errors[url] = e
+    end
+  end
+
+  erb :index
+end
